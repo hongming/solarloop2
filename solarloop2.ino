75438c219e655fde150846e2665e3d531c681ba0
Gerald Chu <hongming@gmail.com>
2015-06-05 14:55:06 +0800

    修改led位置计算方法
    参考Code中的二进制换算法

---
+++ solarloop2.ino
@@ -0,0 +1,62 @@
+//DS1307 library from https://github.com/adafruit/RTClib
+//ShiftOut for 74HC595 http://arduino.cc/en/Reference/ShiftOut
+//ShiftOut Example http://arduino.cc/en/Tutorial/ShiftOut
+//Sundata https://github.com/DataPanik/Sundata
+
+#include "Wire.h"
+#include "RTClib.h"
+#include "sundata.h"
+float loop_sunrise;
+float loop_sunset;
+float loop_currenttime;
+float loop_daylength;
+float loop_timelag = 0;
+int loop_leds = 8;
+int led_current;
+int loop_pin_latch = 8;
+int loop_pin_clock = 12;
+int loop_pin_data = 11;
+
+RTC_DS1307 rtc;
+sundata solarloop = sundata(31.2, 121.2, 8);
+
+void setup() {
+  pinMode(loop_pin_latch, OUTPUT);
+  pinMode(loop_pin_clock, OUTPUT);
+  pinMode(loop_pin_data, OUTPUT);
+  Serial.begin(9600);
+  Wire.begin();
+  rtc.begin();
+  //      rtc.adjust(DateTime(F(__DATE__),F(__TIME__)));
+
+  if (!rtc.isrunning()) {
+    Serial.println("Check your RTC!");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+
+}
+
+void loop() {
+  //获得当前时间
+  DateTime loopnow = rtc.now();
+
+  //为sundata插入当前时间，并进行计算，获得日出时间、日落时间
+  solarloop.time(loopnow.year(),loopnow.month(),loopnow.day(),loopnow.hour(),loopnow.minute(),loopnow.second());
+  solarloop.calculations();
+  loop_sunrise = solarloop.sunrise_time();
+  Serial.print(loop_sunrise);
+  Serial.print("--");
+  loop_sunset = solarloop.sunset_time();
+  Serial.print(loop_sunset);
+  Serial.print("--");
+  //当前时间，以十进制显示
+  loop_currenttime = loopnow.hour() + loopnow.minute() / 60 + loopnow.second() / 3600;
+    Serial.print(loop_currenttime);
+    Serial.print(loopnow.unixtime());
+  //以二进制反推，计算当前时间对应的led亮灯次序
+  led_current = pow(2,(round(loop_currenttime-loop_sunrise)/((loop_sunset-loop_sunrise)/loop_leds)));
+    Serial.print(led_current);
+  //数据导出
+  shiftOut(loop_pin_data, loop_pin_clock, MSBFIRST, highByte(led_current));
+  shiftOut(loop_pin_data, loop_pin_clock, MSBFIRST, lowByte(led_current));
+}
